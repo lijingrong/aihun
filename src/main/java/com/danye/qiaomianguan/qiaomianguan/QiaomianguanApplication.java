8c52e194f57e9ae9ad139fecb4b3cf54730863e1
李景荣 <ljr@promote.cache-dns.local>
2018-04-08 20:47:03 +0800

    Initial commit

---
+++ src/main/java/com/danye/qiaomianguan/qiaomianguan/QiaomianguanApplication.java
@@ -0,0 +1,12 @@
+package com.danye.qiaomianguan.qiaomianguan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiaomianguanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QiaomianguanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/danye/qiaomianguan/qiaomianguan/QiaomianguanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.danye.qiaomianguan.qiaomianguan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QiaomianguanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
