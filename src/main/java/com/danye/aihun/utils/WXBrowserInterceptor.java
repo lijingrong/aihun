36de7ec20bdd9fe051f97dd698ecce05811368ae
hj200724 <huangjiang1026@163.com>
2018-05-04 09:28:49 +0800

    1、访问请求中不包含 User-Agent 并且不是微信浏览器的直接跳到非微信操作页面

--- src/main/java/com/danye/aihun/utils/WXBrowserInterceptor.java
+++ src/main/java/com/danye/aihun/utils/WXBrowserInterceptor.java
@@ -1,5 +1,6 @@
 package com.danye.aihun.utils;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -15,7 +16,8 @@ public class WXBrowserInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        if (request.getHeader("User-Agent").contains("MicroMessenger")) {
+        if (StringUtils.isNotEmpty(request.getHeader("User-Agent"))
+                && request.getHeader("User-Agent").contains("MicroMessenger")) {
             return true;
         } else {
             request.getRequestDispatcher("/noWechat").forward(request, response);
