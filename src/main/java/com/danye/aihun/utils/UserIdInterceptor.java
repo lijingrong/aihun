fad23999f94f1582a545392f877dff9f7c8006e4
hj200724 <huangjiang1026@163.com>
2018-05-09 16:28:10 +0800

    1、UserIdInterceptor 拦截器判断 userId 为空或者非用户，返回 false

--- src/main/java/com/danye/aihun/utils/UserIdInterceptor.java
+++ src/main/java/com/danye/aihun/utils/UserIdInterceptor.java
@@ -1,6 +1,9 @@
 package com.danye.aihun.utils;
 
+import com.danye.aihun.model.User;
+import com.danye.aihun.service.UserService;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -10,12 +13,21 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class UserIdInterceptor extends HandlerInterceptorAdapter {
 
+    @Autowired
+    private UserService userService;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        /*if (StringUtils.isEmpty(request.getParameter("userId")))
-            return false;*/
-        System.out.println("####### userId=" + request.getParameter("userId"));
-        UserIdHolder.setUserId(request.getParameter("userId"));
+        String userId = request.getParameter("userId");
+        if (StringUtils.isEmpty(userId)) {
+            return false;
+        }
+        User user = userService.getUser(userId);
+        if (null == user) {
+            return false;
+        }
+        System.out.println("####### userId=" + userId);
+        UserIdHolder.setUserId(userId);
         return true;
     }
 
